414322447eb4377dbb00a9e31572bc6e2657c395
safdar211ali <safdar211@hotmail.com>
2024-11-22 16:34:45 +0500

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## General Voucher V2
+
+this version 2 for general voucher
+
+#### License
+
+mit
---
+++ general_voucher_v2/config/docs.py
@@ -0,0 +1,10 @@
+"""
+Configuration for docs
+"""
+
+# source_link = "https://github.com/[org_name]/general_voucher_v2"
+# headline = "App that does everything"
+# sub_heading = "Yes, you got that right the first time, everything"
+
+def get_context(context):
+	context.brand_html = "General Voucher V2"
